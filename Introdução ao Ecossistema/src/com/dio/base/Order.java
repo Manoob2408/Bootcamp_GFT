803355bcf066eb5e894c36a3e6870803a941c1e0
Manu <lopez.emanuela@gmail.com>
2022-01-22 02:20:50 -0300

    Javadoc

--- Introdução ao Ecossistema/src/com/dio/base/Order.java
+++ Introdução ao Ecossistema/src/com/dio/base/Order.java
@@ -1,11 +1,30 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
+/**
+ * Representa um pedido
+ * 
+ * @author Emanuela Lopez
+ * @version 1.0.0
+ * @see BigDecimal
+ * @since Release 1.0.0
+ */
 public class Order {
 
     private final String code;
+    private final BigDecimal totalValue;
+    private String[] items;
 
-    public Order(String code) {
+    /**
+     * Construtor da classe
+     * 
+     * @param code       Código do pedido
+     * @param totalValue Total do pedido
+     */
+    public Order(String code, BigDecimal totalValue) {
         this.code = code;
+        this.totalValue = totalValue;
     }
 
     @Override
@@ -14,4 +33,50 @@ public class Order {
                 "code='" + code + "'" +
                 "}";
     }
+
+    /**
+     * Calcula as taxas de acordo com o valor total do pedido. Se o valor for maior
+     * que 100, uma taxa será cobrada
+     * 
+     * @return Valor total do pedido com as taxas calculadas
+     * @throws RuntimeException Se o pedido for um valor negativo
+     */
+    public BigDecimal calculateFee() throws RuntimeException {
+        if (this.totalValue.signum() < 0) {
+            throw new RuntimeException("O pedido não pode ter valor negativo!");
+        }
+        if (this.totalValue.compareTo(new BigDecimal("100.00")) > 100) {
+            return this.totalValue.multiply(new BigDecimal("0.99"));
+        }
+        return this.totalValue;
+    }
+
+    public void printItems() {
+        // int i = 0;
+        /*
+         * while (i < items.length) {
+         * System.out.println(items[i]);
+         * i++;
+         * };
+         */
+
+        /*
+         * do{
+         * System.out.println(items[i]);
+         * i++;
+         * }while(i < items.length);
+         */
+
+        /*
+         * for(int i=0; i< items.length; i++)
+         * {
+         * System.out.println(items[i]);
+         * }
+         */
+
+        for (String i : items) {
+            System.out.println(i);
+        }
+    }
+
 }
