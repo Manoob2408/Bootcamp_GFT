64a73f6f53542b6ac34e6d762e5ca4eb36f20fbe
Manu <lopez.emanuela@gmail.com>
2022-01-25 03:14:37 -0300

    New Module Tipos

---
+++ Variáveis/src/Tipos.java
@@ -0,0 +1,53 @@
+public class Tipos {
+
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        // short s2 = 40000;
+
+        //int i1= 10000000000;
+        int i2 = 28250;
+
+        long l1 = 1000000000000000000L;
+        // long l2 = 200000000000000000000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.64D;
+
+        char c1 = 'W';
+        // char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "Um ninho de mafagafos cheio de mafagafinhos";
+
+        // String dt1 = "01/01/2001"; Tipo de dado errado
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+    }
+
+
+}
